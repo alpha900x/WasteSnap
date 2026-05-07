3a529b559408115f5bce66fd66dd92db09d13513
Atharva Solkar <134603916+alpha900x@users.noreply.github.com>
2026-05-07 09:12:52 +0530

    Integrate React Query and Wouter for routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Router } from "wouter";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <App />
+    </Router>
+  </QueryClientProvider>
+);
