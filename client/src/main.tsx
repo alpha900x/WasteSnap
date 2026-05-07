3cc7be8f67e104046d2ed74644850819deeb3c16
Atharva Solkar <134603916+alpha900x@users.noreply.github.com>
2026-05-07 09:16:59 +0530

    Remove app initialization and add boot log
    Removed React app initialization code and added a console log.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,1 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Router } from "wouter";
-
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <Router>
-      <App />
-    </Router>
-  </QueryClientProvider>
-);
+console.log("BOOT OK");
